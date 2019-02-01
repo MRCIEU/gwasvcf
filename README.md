66a3c699442c220adfd0be32e0a892d77a51600a
Gibran Hemani <explodecomputer@gmail.com>
2019-02-01 13:27:45 +0000

    updated

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
-todo:
+# gwasvcftools
 
-check for chr:pos snp lists
+Some utilities to create and query GWAS BCF files. These files are used to store GWAS summary data, and are not used to store genotype data. They are used for the utility of being able to quickly extract metadata from the fixed fields.
+
+## Example
+
+In the file `/tests/harmonise_against_ref.r` shows an example script for creating a BCF file that is harmonised against a VCF reference file. The objective here is to use a common human genome build such that the reference allele is the same across all studies.
+
+To run `/tests/harmonise_against_ref.r`:
+
+```
+Rscript harmonise_against_ref.r \
+--ref_file /mnt/storage/private/mrcieu/research/mr-eve/vcf-reference-datasets/1000g/1kg_v3_nomult.bcf \
+--ref_build b37 \
+--ref_info "1000 genomes phase 3 variants generated using https://github.com/MRCIEU/vcf-reference-datasets" \
+--mrbase_id 2 \
+--gwas_file /mnt/storage/private/mrcieu/research/mr-eve/gwas-files/2/elastic.gz \
+--delimiter $'\t' \
+--gzipped 1 \
+--skip 0 \
+--dbsnp_field 1 \
+--ea_field 2 \
+--nea_field 3 \
+--ea_af_field 4 \
+--effect_field 5 \
+--se_field 6 \
+--pval_field 7 \
+--n_field 8 \
+--out /mnt/storage/private/mrcieu/research/mr-eve/gwas-files/2/harmonised \
+--out_type bcf
+```
 
---
+++ tests/harmonise_against_ref.r
@@ -0,0 +1,104 @@
+#!/usr/bin/env Rscript
+suppressPackageStartupMessages({
+	library(argparse)
+	library(dplyr)
+	library(TwoSampleMR)
+	library(gwasvcftools)
+	library(unixtools)
+})
+
+library(devtools)
+load_all()
+
+# create parser object
+parser <- ArgumentParser()
+parser$add_argument('--ref_file', required=TRUE)
+parser$add_argument('--ref_build', required=TRUE)
+parser$add_argument('--ref_info', required=TRUE)
+parser$add_argument('--mrbase_id', required=TRUE)
+parser$add_argument('--gwas_file', required=TRUE)
+parser$add_argument('--gzipped', required=TRUE, type="integer", default=1)
+parser$add_argument('--delimiter', default="\t", required=TRUE)
+parser$add_argument('--skip', required=TRUE, type="integer", default=0)
+parser$add_argument('--dbsnp_field', type="integer", required=TRUE)
+parser$add_argument('--ea_field', type="integer", required=TRUE)
+parser$add_argument('--nea_field', type="integer", required=FALSE, default=0)
+parser$add_argument('--ea_af_field', type="integer", required=FALSE, default=0)
+parser$add_argument('--effect_field', type="integer", required=FALSE, default=0)
+parser$add_argument('--se_field', type="integer", required=FALSE, default=0)
+parser$add_argument('--pval_field', type="integer", required=FALSE, default=0)
+parser$add_argument('--n_field', type="integer", required=FALSE, default=0)
+parser$add_argument('--info_field', type="integer", required=FALSE, default=0)
+parser$add_argument('--z_field', type="integer", required=FALSE, default=0)
+parser$add_argument('--out_type', required=TRUE, default="bcf")
+parser$add_argument('--out', required=TRUE)
+args <- parser$parse_args()
+str(args)
+
+
+# Read in GWAS data
+set.tempdir("tmp")
+gwas <- read_gwas(
+	args[["gwas_file"]],
+	skip=args[["skip"]],
+	snp=args[["dbsnp_field"]],
+	gzipped=args[["gzipped"]],
+	delimiter=args[["delimiter"]],
+	ea=args[["ea_field"]],
+	nea=args[["nea_field"]],
+	ea_af=args[["ea_af_field"]],
+	effect=args[["effect_field"]],
+	se=args[["se_field"]],
+	pval=args[["pval_field"]],
+	n=args[["n_field"]],
+	info=args[["info_field"]],
+	z=args[["z_field"]]
+)
+
+
+# Read in ref
+ref <- read_reference(args[["ref_file"]], gwas$SNP, args[["out"]]) 
+
+# Harmonise
+harmonised <- harmonise_against_ref(gwas, ref)
+
+# Gather metadata
+metadata.input <- args
+names(metadata.input) <- paste0("input.", names(metadata.input))
+
+ao <- TwoSampleMR::available_outcomes(NULL)
+metadata.gwas <- as.list(subset(ao, id == args[["mrbase_id"]]))
+names(metadata.gwas) <- paste0("gwas.", names(metadata.gwas))
+metadata.gwas[['path']] <- NULL
+metadata.gwas[['filename']] <- NULL
+
+metadata.counts <- as.list(attr(harmonised, "log"))
+names(metadata.counts) <- paste0("counts.", names(metadata.counts))
+matadata.counts[['id.exposure']] <- NULL
+matadata.counts[['id.outcome']] <- NULL
+
+metadata <- c(metadata.input, metadata.gwas, metadata.counts)
+str(metadata)
+
+# Create vcf format
+vcf <- gwasvcftools::make_vcf(
+	ID = harmonised$ID,
+	ALT = harmonised$ALT,
+	REF = harmonised$REF,
+	B = harmonised$BETA,
+	SE = harmonised$SE,
+	PVAL = harmonised$PVALUE,
+	N = harmonised$N,
+	CHROM = harmonised$CHROM,
+	POS = harmonised$POS,
+	AF = harmonised$AF,
+	QUAL = harmonised$INFO,
+	FILTER = rep("PASS", nrow(harmonised)),
+	ZVALUE = harmonised$ZVALUE,
+	build = args[["ref_build"]],
+	meta_data = metadata
+)
+
+# Write vcf
+gwasvcftools::write_vcf(vcf, paste0(args[["out"]], ".", args[["out_type"]]))
+
