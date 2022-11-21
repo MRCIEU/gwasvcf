ff14dad8d5be1589d3de31cea2f70a7e8f072757
Tom Palmer <remlapmot@hotmail.com>
2022-11-21 08:04:55 +0000

    Skip tests on Windows

--- tests/testthat/test_pvali.r
+++ tests/testthat/test_pvali.r
@@ -3,26 +3,30 @@ library(gwasvcf)
 
 fn <- system.file("extdata","data.vcf.gz", package="gwasvcf")
 vcf <- VariantAnnotation::readVcf(fn)
-set_bcftools()
+if (Sys.info()["sysname"] != "Windows") set_bcftools()
 
 indexname <- tempfile()
 
 test_that("create index", {
+  skip_on_os("windows")
 	create_pval_index_from_vcf(fn, 0.4, indexname)
 	expect_true(file.exists(indexname))
 })
 
 test_that("read in", {
+  skip_on_os("windows")
 	out <- query_pvali(0.05, indexname)
 	expect_equal(nrow(out), 7)
 })
 
 test_that("query with pvali", {
+  skip_on_os("windows")
 	b <- query_gwas(fn, pval=0.05, pvali=indexname)
 	expect_equal(nrow(b), 7)
 })
 
 test_that("query with pvali", {
+  skip_on_os("windows")
 	b <- query_gwas(fn, pval=0.05)
 	expect_equal(nrow(b), 7)
 })
